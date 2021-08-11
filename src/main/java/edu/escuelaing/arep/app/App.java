138489540578f23930b2a47aca5060c80839abdd
JC <juancamiloposso01@gmail.com>
2021-08-11 18:39:40 -0500

    pom dependences and procfile

--- src/main/java/edu/escuelaing/arep/app/App.java
+++ src/main/java/edu/escuelaing/arep/app/App.java
@@ -1,13 +1,12 @@
 package edu.escuelaing.arep.app;
-
+import static spark.Spark.*;
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!!" );
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello Heroku");
     }
 }
