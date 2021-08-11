622ac44242c5ff87c89684840504f7b7a4ca5b66
JC <juancamiloposso01@gmail.com>
2021-08-11 18:59:48 -0500

    add bootstrap

--- src/main/java/edu/escuelaing/arep/app/App.java
+++ src/main/java/edu/escuelaing/arep/app/App.java
@@ -7,6 +7,21 @@ import static spark.Spark.*;
 public class App 
 {
     public static void main(String[] args) {
+        port(getPort());
+        staticFiles.location( "/public" );
         get("/hello", (req, res) -> "Hello Heroku");
+    }
+    /**
+     * This method reads the default port as specified by the PORT variable in
+     * the environment.
+     *
+     * Heroku provides the port automatically so you need this to run the
+     * project on Heroku.
+     */
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; //returns default port if heroku-port isn't set (i.e. on localhost)
     }
 }
